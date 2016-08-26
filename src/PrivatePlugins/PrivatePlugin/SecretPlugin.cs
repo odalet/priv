06317563907d820c5ffb36f6dfe00a1601442a02
Olivier DALET <odalet@gmail.com>
2016-08-26 21:21:07 +0200

    Very complex plugin!

---
+++ src/PrivatePlugins/PrivatePlugin/SecretPlugin.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using HostApi;
+
+namespace PrivatePlugin
+{
+    internal class SecretPlugin : Plugin
+    {
+        public override void Run() =>
+            Console.WriteLine(@"This plugin's source code is very precious: 
+It was only possible thanks to 10+ years of intensive R&D in the domain of text output. 
+It should definitely never make it into the wild!");
+    }
+}
